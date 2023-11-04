aee5b99fe1f67f17c004a9ee55f13030566800b2
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2023-11-04 11:07:23 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,34 +15,29 @@ Validate their operation choice. If it's not one of the choices, print an error 
 You must use the switch operator in your solution to receive full credit
  */
 #include <iostream>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
 #include <unistd.h>
-#include <limits>
 #include <stdlib.h>
 
 using namespace std;
 
-void userWelcome();
+void userWelcome();//greeting
 
-int validation_integer(int min, int max);
+int validation_integer(int min, int max);//validation
 
-int num_1();
+int num_1();//user number
 
-int choice();
+int choice();//user's math choice
 
-int exponent(int N);
+int exponent(int N);//loop to calculate exponent
 
 
 int main() {
     int N;//user number
-    int userChoice;
+    int userChoice;//user's math choice
     int result; // variables for calculation results
-    //userWelcome();
-    N = num_1();
-    userChoice = choice();
+    userWelcome();//greeting
+    N = num_1();//assign N to user's validated number
+    userChoice = choice();//assign variable to user's validated choice
 
     //performCalculation;
     switch (userChoice) {
@@ -61,23 +56,23 @@ int main() {
             else if (N == 1) {
                 result = 2;
             }
-            else{
-        int base = 2;
-        for (int i = 2; i <=N; ++i) {
-            base *= 2;
-        }
-            result = base;
-        }
+            else//exponsent calculator
+            {
+                int base = 2;
+                for (int i = 2; i <= N; ++i) {
+                    base *= 2;
+                }
+                result = base;
+            }
 
 
     }
 
-            cout << "The result of your calculation is: " << result << ". Thank you for playing!";
+    cout << "The result of your calculation is: " << result << ". Thank you for playing!";
 
 
-            return 0;
-    }
-
+    return 0;
+}
 
 
 int num_1() {
@@ -86,7 +81,8 @@ int num_1() {
     return validation_integer(min, max);
 }
 
-int choice() {
+int choice() //get user's choice//
+{
     cout << "Please enter your choice as a number 1 -3: " << endl;
     cout << "1. 2 * N" << endl << "2. N^2" << endl << "3. 2^N" << endl;
     int min = 1, max = 3;
@@ -108,14 +104,15 @@ int validation_integer(int min, int max) //function to validate integers
     return number;
 }
 
-void userWelcome() {
+void userWelcome() //greeting
+{
     string user_name;
     cout << "Welcome to Carrie's Number Calculator! What is your name? ";
     cin >> user_name;
-    cout << "Hi " << user_name
-         << "I'm going to ask you for a number between 1 and 10 and a "
-            "choice of three mathematical operations. Then I will perform "
-            "the appropriate calculation.";
+    cout << "Hi " << user_name << "!" << endl;
+    cout << "I'm going to ask you for a number between 1 and 10 and a "
+       "choice of three mathematical operations. Then I will perform "
+       "the appropriate calculation.";
     sleep(2);
     cout << "So, let's play!" << endl;
 
